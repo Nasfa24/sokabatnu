ee6c1629adf82ed9b26c5edc3c2e64af8403ab93
Nasfa24 <podcastmiliksaya@gmail.com>
2026-09-02 20:16:45 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,12 @@
 const CACHE_NAME = 'ecms-v5-cache';
-const urlsToCache = [ '/', '/index.html', '/caleg.html', '/tim.html', '/inputer.html', '/favicon-32.png' ];
+const urlsToCache = [ '/', '/index.html', '/caleg.html', '/tim.html', '/inputer.html' ];
 
 self.addEventListener('install', event => {
   event.waitUntil( caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)) );
 });
 self.addEventListener('fetch', event => {
-  event.respondWith( caches.match(event.request).then(response => response || fetch(event.request)) );
+  // Network First, Fallback to Cache Strategy (Mencegah tampilan tertahan di versi lama)
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
